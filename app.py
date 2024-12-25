39a54c32279319b510576f31568f4a6c521bfd59
Milad <Milad Ghasembeigi>
2024-12-25 17:58:20 +1100

    Colouring added, Ui updated, Network trained

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ from tkinter import PhotoImage
 from PIL import Image, ImageTk
 
 import numpy as np
+import random #USed to shuffle training batches
 from main import Neural_Network
 
 def main():
     window = tk.Tk()
     window.title("Cornelius | Neural Network")
-    window.resizable(False, False)
+    window.resizable(True, True)
 
 
     #Canvas is square
     n_pixels = 30 
 
 
-    canvas_size = 15 #Affects x and y
+    canvas_size = 25 #Affects x and y
     canvas_size_y = n_pixels * canvas_size 
     canvas_size_x = canvas_size_y * 1.05 
-
+    print(canvas_size_y)
     canvas_x_offset = 30
     canvas_y_offset = 20
     window.focus_force()
@@ -55,11 +56,9 @@ def main():
     def load_click(i):
         e = nn.load_model()
         if e == 0:
-            i.config(state="disabled")
             log("Loaded saved model from /trained_model directory")
         elif e == 1:
             log("ERROR: Model is missing from /trained_model directory. Cannot load.")
-
 
 
 
@@ -106,14 +105,14 @@ def main():
     title = tk.Label(options_frame, text="Controls", font=("Arial",18))
     title.grid(row=1, column=0, padx=0, pady=10)
 
-    cmd_label = tk.Label(options_frame, text="• Left-click to draw\n• Right-click to erase\n• 'R' to reset", font=("Arial",14))
+    cmd_label = tk.Label(options_frame, text="• Left-click to draw\n• Right-click to erase\n• 'R' to clear canvas\n• 'L' to load training material", font=("Arial",14))
     cmd_label.grid(row=2, column=0, padx=0, pady=0)
 
 
 
     #Slider Intensity
     label = tk.Label(options_frame, text="Draw Intensity")
-    label.grid(row=3, column=0, padx=0, pady=(50,0),sticky="n")
+    label.grid(row=3, column=0, padx=0, pady=(10,0),sticky="n")
     intensity_slider = tk.Scale(options_frame, from_=0, to=255, orient="horizontal")
     intensity_slider.grid(row=4, column=0, padx=0,pady=0)
 
@@ -129,10 +128,13 @@ def main():
     
 
 
-    #Load model button
-    button = tk.Button(options_frame, text="Load Model",state="normal", command=lambda : load_click(button))
-    button.grid(row=10, column=0, padx=0,pady=(30,15)) 
+    #Run automatically button
+    #button = tk.Button(options_frame, text="Run Automatically: ON",state="normal")
+    #button.config(command=lambda : toggle_automatic(button))
+    #button.grid(row=10, column=0, padx=0,pady=(30,0)) 
 
+    #button = tk.Button(options_frame, text="Run Network",state="disabled", command=lambda : load_click(button))
+    #button.grid(row=11, column=0, padx=0,pady=(0,15))
 
 
 
@@ -141,14 +143,27 @@ def main():
 
     #Log
     log_Title = tk.Label(options_frame, text="Neural Network Log:", font=("Arial",18))
-    log_Title.grid(row=100, column=0, padx=0, pady=(0,0), sticky="s")
+    log_Title.grid(row=100, column=0, padx=0, pady=(10,0), sticky="s")
 
-    log_label = tk.Label(options_frame, text="App launched", font=("Andale Mono",15),wraplength=180)
+    log_label = tk.Label(options_frame, text="App launched", font=("Andale Mono",15),wraplength=180, justify="left")
     log_label.grid(row=101, column=0, padx=(10,0), pady=0, sticky="w")
 
 
+
+
+
     #Display to log
+    log_history = []
     def log(text):
+        '''
+        log_history.append(text)
+        if len(log_history) >= 4:
+            log_history.pop(0)
+
+        log_text = ""
+        for i in log_history:
+            log_text+= "• "+l +"\n\n"
+        '''
         log_label.config(text=text)
 
         if text.startswith("ERROR:"):
@@ -157,6 +172,16 @@ def main():
             log_label.config(fg="yellow")
         else:
             log_label.config(fg=log_Title.cget("fg"))
+
+
+
+
+    def on_resize(event):
+        pass
+        #todo make it change on rezise SNOOOZE*
+
+
+
 
 
 
@@ -178,6 +203,7 @@ def main():
     load_click(button)
 
 
+
     class box:
         def __init__(self, weight,id):
             self.weight = weight
@@ -192,8 +218,21 @@ def main():
             self.probability = 255
             self.colour = "black" #Stores desired colour of the entire canvas
         
-        def set_colour(self,colour):
-            self.colour = colour
+        def set_colour(self,index):
+            if index == 0:
+                self.colour = "green"
+            elif index == 1:
+                self.colour = "yellow"
+            elif index == 2:
+                self.colour = "red"
+            elif index == 3:
+                self.colour = "blue"
+            elif index == 4:
+                self.colour = "purple"
+            else:
+                self.colour = "black"
+
+
 
         def set_probability(self, probability):
             self.probability = probability *255 # as decimal
@@ -201,22 +240,49 @@ def main():
 
 
         def calculate_colour(self,weight):
-            if self.colour == "black":
+            if self.colour == "black" or self.probability < 255 - weight:
                 self.r = 255 - weight
                 self.g = 255 - weight
                 self.b = 255 - weight
             
+            elif self.colour == "green":
+                self.r = 255 - weight
+                self.g = self.probability
+                self.b = 255 - weight
 
+            elif self.colour == "yellow":
+                self.r = self.probability
+                self.g = self.probability
+                self.b = 255 - weight
 
             elif self.colour == "red":
                 self.r = self.probability
                 self.g = 255 - weight
                 self.b = 255 - weight
+
+            elif self.colour == "blue":
+                self.r = 255 - weight
+                self.g = 255 - weight
+                self.b = self.probability
+
+            elif self.colour == "purple":
+                self.r = self.probability
+                self.g = 255 - weight
+                self.b = self.probability
+
+
             
 
         def get(self,weight): #Constructs Hexcode colour eg #422AF0
-            self.calculate_colour(weight)
-            full_str = "#"
+            if weight == 0: #Skip logic return white, for erasing
+                return "#ffffff" 
+
+            self.calculate_colour(weight) #Prepares r g b values
+
+            full_str = "#" #Begins construcing hex code
+
+
+
             hex_str = hex(self.r)[2:4]
             if len(hex_str) == 1: #Solves bug with hex formatting
                 hex_str = "0"+hex_str
@@ -245,7 +311,7 @@ def main():
                 rect = canvas.create_rectangle(
                     i * pixel_size + canvas_x_offset, j * pixel_size + canvas_y_offset,
                     (i + 1) * pixel_size + canvas_x_offset, (j + 1) * pixel_size + canvas_y_offset,
-                    fill="#FFFFFF" , outline="purple"
+                    fill="#FFFFFF" , outline="black"
                 )
                 #Store coordinates to list
                 canvas_list.append(box(0, rect))
@@ -263,32 +329,6 @@ def main():
         weight = min(weight , 255)
         weight = max(weight, 0)
         return weight
-
-    def calculateColour(weight,colour_index, probability): #Returns String hex code eg #FFFFFF
-
-        hex_str =  hex(255-weight)[2:4] #Calculate plain colour
-        
-        #certainty_str = hex(int((255- (probability/100) *255))  )[2:4] #Calculate hue
-        certainty_str = "00"
-        if probability > 0:
-            certainty_str = "00" 
-
-
-        if len(hex_str) == 1: #Solves bug with hex formatting
-            hex_str = "0"+hex_str
-        if len(certainty_str) == 1: #Example: #999 -> #090909
-            certainty_str = "0"+certainty_str
-
-        if colour_index == "black": #Default black
-            return "#"+hex_str * 3 
-
-        if weight > 0:
-            if colour_index == 0: #Green
-                return "#"+hex_str * 3
-
-            if colour_index == 2: #Red
-                return "#"+hex_str + certainty_str *2
-
 
 
 
@@ -321,9 +361,14 @@ def main():
     def change_colours(index,val):
         
         if(val > 40):
-            colour.set_colour("red")
+            colour.set_colour(index)
             colour.set_probability(val.item()/100)
-            for box in drawn_list:
+            
+        else:
+            colour.set_colour("black")
+            colour.set_probability(0)
+
+        for box in drawn_list:
                 new_colour = colour.get(box.weight)
                 canvas.itemconfig(box.id, fill=new_colour)
 
@@ -332,14 +377,15 @@ def main():
 
     def call_network(inputs):
         nn.run_network(inputs)
-        log("Network ran sucessfully")
         for i in range(len(prediction_texts)): #Update UI text
             text_string = "%.2f%c"%(nn.probabilities[0][i] *100,'%')
             prediction_texts[i].config(prediction_texts[i], text=text_string)
 
+        #Get max probability and call change colours
         max_val = max(nn.probabilities[0])
         index = np.where(nn.probabilities[0] == max(nn.probabilities[0]))[0][0]
-        #change_colours(index, max_val * 100)
+        change_colours(index, max_val * 100)
+        log("Network ran sucessfully")
 
 
 
@@ -352,7 +398,7 @@ def main():
 
         nn.propagate_backward()
         nn.save_model()
-
+        export()
         
         log("Trained and saved model: %s"%(index_dict[i]))
 
@@ -365,23 +411,41 @@ def main():
         for pixel in canvas_list:
             pixel_arr.append(round((pixel.weight / 255.0),2))
 
-        text_string = "training_material/4_6.npy"
+        text_string = "training_material/4_15.npy"
         np.save(text_string , pixel_arr)
         log("Saved data: %s"%(text_string))
 
 
     '''
+    Batches are stored in training_material/ directory
+    file names are in format (index)_(batchnumber).npy, where index is desired drawing
+    eg, 4_5.npy is A triangle drawing from the 5th batch
+
     #Batch #1 is drawn as in the reference image
     #Batch #2 is drawn in the middle of canvas
     #Batch #3 is drawn at the bottom of canvas
     #Batch #4 is drawn large and shaded in 
     #Batch #5 is drawn smaller and shaded in, (Eyes were instead made close and far)
     #Batch #6 is drawn very thin (1 thickness)
+    #Batch #7 is drawn left side of canvas
+    #Batch #8 is drawn right side of canvas
+    #Batch #9 is drawn top left corner
+    #Batch #10 is drawn top right corner
+    #Batch #11 is drawn bottom left corner
+    #Batch #12 is drawn bottom right corner
+    #Batch #13 is drawn in the top third
+    #Batch #14 is drawn in the bottom third
+    #Batch #15 is drawn in the middle, small
     '''
 
     def load_data(event): 
         trained_amount = 0
-        for i in range(1,7): #Batch
+        training_set = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        #training_set = [1,2,3,4]
+        random.shuffle(training_set)
+        old_rate = nn.learning_rate
+        nn.learning_rate = 0.001
+        for i in training_set: #Batch
             for ii in range(5): #All 5  inputs
                 filename = "training_material/%d_%d.npy"%(ii,i)
                 a = np.load(filename)
@@ -389,6 +453,7 @@ def main():
                 back_propagate(ii) #Back propagate with index
                 trained_amount += 1
 
+        nn.learning_rate = old_rate
         erase(0)
         log("Trained %d samples of data!"%(trained_amount))
 
@@ -415,21 +480,20 @@ def main():
             
             target = canvas_list[mouse_y + mouse_x* n_pixels]
 
-            prior_weight = calculateWeight(canvas.itemcget(target.id, "fill"))
+            prior_weight = target.weight
             new_weight = addWeights(prior_weight, weight)
             
-            if prior_weight == 0 and new_weight > 0: #Cool feature but hurts performance, dont care 
+            if prior_weight == 0 and new_weight > 0: #Cool feature to improve performance, dont care 
                 drawn_list.append(target) #append to list
             if prior_weight > 0 and new_weight == 0:
                 drawn_list.remove(target) 
             
             target.weight = new_weight
-            #new_color = calculateColour(new_weight,"black",0)
-            canvas.itemconfig(target.id, fill=colour.get(new_weight))  # Change the fill color to black
+            canvas.itemconfig(target.id, fill=colour.get(new_weight))  # Change the fill, use new colour
             
 
 
-            if draw_thickness > 0:
+            if draw_thickness > 0 and draw_intensity != 0:
                 weight_loss = (draw_intensity // draw_thickness)
                 spread_weight = weight - weight_loss
 
@@ -464,10 +528,12 @@ def main():
     canvas.bind("<r>", erase)
 
     #O to export
-    canvas.bind("<o>", save_data)
+    #canvas.bind("<o>", save_data)
     
     canvas.bind("<l>", load_data)
     
+
+    window.bind("<Configure>", on_resize)
 
     canvas.focus_set()
 
